2c7a2064e043745e355100c9acdea6a6597c5bf4
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2025-03-31 16:03:55 +0530

    Implemented Default Selected Item in Sidebar config

--- src/lib/store/appStore.ts
+++ src/lib/store/appStore.ts
@@ -1,29 +1,36 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { storage } from "../storage";
+import { immer } from "zustand/middleware/immer";
+import { EFavouriteGroup } from "@/constants/default";
 
 interface AppStoreState {
-    defaultSelectedDir: string | null;
+  defaultSelectedDir: EFavouriteGroup;
 }
 
-interface AppStoreActions {}
+interface AppStoreActions {
+  setDefaultSelectedDir: (item: EFavouriteGroup) => void;
+}
 
 const initialAppState: AppStoreState = {
-    defaultSelectedDir: "recent",
+  defaultSelectedDir: EFavouriteGroup.RECENTS,
 };
 
 export const useAppStore = create<AppStoreState & AppStoreActions>()(
-    persist(
-        () => ({
-            ...initialAppState,
-            // Implement your action methods here
+  persist(
+    immer((set) => ({
+      ...initialAppState,
+      setDefaultSelectedDir: (dir: EFavouriteGroup) =>
+        set((state) => {
+          state.defaultSelectedDir = dir;
         }),
-        {
-            name: "appStore",
-            partialize: (state) => ({
-                defaultSelectedDir: state.defaultSelectedDir,
-            }),
-            storage: createJSONStorage(() => storage),
-        },
-    ),
+    })),
+    {
+      name: "appStore",
+      partialize: (state) => ({
+        defaultSelectedDir: state.defaultSelectedDir,
+      }),
+      storage: createJSONStorage(() => storage),
+    },
+  ),
 );
--- src/lib/store/sidebarStore.ts
+++ src/lib/store/sidebarStore.ts
@@ -1,35 +1,67 @@
+import {
+  defaultSidebar,
+  ESidebarGroup,
+  type ISidebarItem,
+  type SidebarType,
+} from "@/constants/default";
+import { IDisk } from "@/types/system";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { storage } from "../storage";
-import { defaultSidebar, ISidebarItem } from "@/constants/default";
 import { immer } from "zustand/middleware/immer";
-import { IDisk } from "@/types/system";
+import { storage } from "../storage";
+import { useAppStore } from "./appStore";
 
 interface SidebarStates {
-    activeItem: ISidebarItem | IDisk;
+  activeItem: ISidebarItem | IDisk;
+  sidebarData: SidebarType;
 }
 
 interface SidebarActions {
-    setActiveItem: (item: ISidebarItem | IDisk) => void;
+  setActiveItem: (item: ISidebarItem | IDisk) => void;
+  resetSidebar: () => void;
 }
 
 const initialSidebarState: SidebarStates = {
-    activeItem: defaultSidebar?.["favourites"]?.[0],
+  activeItem: getInitialalActiveItem() as any,
+  sidebarData: defaultSidebar,
 };
 
 export const useSidebarStore = create<SidebarStates & SidebarActions>()(
-    persist(
-        immer((set) => ({
-            ...initialSidebarState,
-            setActiveItem: (item: ISidebarItem | IDisk) =>
-                set((state) => {
-                    state.activeItem = item;
-                }),
-        })),
-        {
-            name: "sidebarStore",
-            partialize: (state) => ({}),
-            storage: createJSONStorage(() => storage),
-        },
-    ),
+  persist(
+    immer((set) => ({
+      ...initialSidebarState,
+      resetSidebar: () => set(() => ({ sidebarData: defaultSidebar })),
+      setActiveItem: (item: ISidebarItem | IDisk) =>
+        set((state) => {
+          state.activeItem = item;
+        }),
+    })),
+    {
+      name: "sidebarStore",
+      partialize: (state) => ({
+        activeItem: state.activeItem,
+        sidebarData: state.sidebarData,
+      }),
+      storage: createJSONStorage(() => storage),
+    },
+  ),
 );
+
+// --- Utilities
+function getInitialalActiveItem(): ISidebarItem | undefined {
+  const { defaultSelectedDir } = useAppStore.getState();
+
+  // --- Default Sidebar Item
+  const defaultSidebarItem = defaultSidebar?.[ESidebarGroup.FAVOURITES]?.find(
+    (item) => item.name === "recents",
+  );
+
+  // --- Selected Sidebar Item
+  const selectedSidebarItem = defaultSidebar?.[ESidebarGroup.FAVOURITES]?.find(
+    (item) => item.name === defaultSelectedDir,
+  );
+
+  return defaultSidebar?.[ESidebarGroup.FAVOURITES]
+    ? selectedSidebarItem
+    : defaultSidebarItem;
+}
