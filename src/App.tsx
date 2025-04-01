06288c1b861162b0591a52e67f5dbbe3ad80ae57
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2025-04-01 14:24:55 +0530

    initiating the `menubar` on initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
+import "@/lib/configs";
 import * as React from "react";
 import Workspace from "./components/custom/workspace/workspace";
+import SettingsDialog from "./components/dialogs/settings/settings";
 import AppLayout from "./components/layouts/app-layout";
 import { TooltipProvider } from "./components/ui/tooltip";
-import "@/lib/configs";
+import { setUpMenu } from "@/lib/configs";
+import { useAppStore } from "./lib/store/appStore";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
+  // -- Store
+  const { activeWindowIndex } = useAppStore();
+  console.log("Active Window", activeWindowIndex);
+
+  // --- Effects
+  React.useEffect(() => {
+    // Initialize app state
+    setUpMenu();
+  }, []);
+
   return (
     <TooltipProvider>
+      {/* Default Screen */}
       <AppLayout>
         <Workspace />
       </AppLayout>
+
+      {/* Settings Modal */}
+      <SettingsDialog />
     </TooltipProvider>
   );
 };
