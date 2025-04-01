ac1d8ae7f4c2f3f927c0b376d568027206a679dd
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2025-04-01 14:20:52 +0530

    re-factoring

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -4,15 +4,12 @@ const store = await load("bfm.json", { autoSave: true });
 
 export const storage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
-    console.log(name, "has been retrieved");
     return (await store.get(name)) || null;
   },
   setItem: async (name: string, value: string): Promise<void> => {
-    console.log(name, "with value", value, "has been saved");
     await store.set(name, value);
   },
   removeItem: async (name: string): Promise<void> => {
-    console.log(name, "has been deleted");
     await store.delete(name);
   },
 };
