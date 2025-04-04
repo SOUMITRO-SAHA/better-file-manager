5aa7f2a74768c690b3b26d3a04d3a361317c5938
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2025-04-04 15:06:19 +0530

    Implemented the Home Icon for the root dir

--- src/components/custom/breadcrumb/breadcrumb-generator.tsx
+++ src/components/custom/breadcrumb/breadcrumb-generator.tsx
@@ -1,27 +1,45 @@
 import {
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { pathChange } from "@/lib/utils/common";
+import { HouseIcon } from "lucide-react";
 import * as React from "react";
 
 interface BreadCrumbGeneratorProps {
   path: string;
 }
 
+type PathSegment = {
+  name: string;
+  href: string;
+};
+
+const defaultHomeDirectory: Readonly<PathSegment> = {
+  name: "Home",
+  href: "/",
+};
+
 const BreadCrumbGenerator: React.FC<BreadCrumbGeneratorProps> = (props) => {
   const { path } = props;
 
-  const pathSegments = path
-    .split("/")
-    .filter((segment) => segment !== "")
-    .map((segment, index, array) => {
-      const href = "/" + array.slice(0, index + 1).join("/");
-      return { name: segment, href };
-    });
+  const pathSegments: PathSegment[] =
+    path
+      .split("/")
+      .filter((segment) => segment !== "")
+      .map((segment, index, array) => {
+        const href = "/" + array.slice(0, index + 1).join("/");
+        return { name: segment, href };
+      }) || [];
+
+  if (pathSegments.length === 0) {
+    pathSegments.push(defaultHomeDirectory);
+  }
 
   return (
     <Breadcrumb>
@@ -31,11 +49,31 @@ const BreadCrumbGenerator: React.FC<BreadCrumbGeneratorProps> = (props) => {
             {index > 0 && <BreadcrumbSeparator />}
             <BreadcrumbItem key={index}>
               {index === pathSegments.length - 1 ? (
-                <BreadcrumbPage>{segment.name}</BreadcrumbPage>
+                <BreadcrumbPage className={cn("cursor-default")}>
+                  {segment.href === "/" ? (
+                    <HouseIcon className="w-4 h-4 text-muted-foreground" />
+                  ) : (
+                    segment.name
+                  )}
+                </BreadcrumbPage>
               ) : (
-                <BreadcrumbLink href={segment.href}>
+                <BreadcrumbItem
+                  className={cn(
+                    buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                      className: "cursor-pointer",
+                    }),
+                  )}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    pathChange({
+                      destinationPath: segment.href,
+                    });
+                  }}
+                >
                   {segment.name}
-                </BreadcrumbLink>
+                </BreadcrumbItem>
               )}
             </BreadcrumbItem>
           </React.Fragment>
